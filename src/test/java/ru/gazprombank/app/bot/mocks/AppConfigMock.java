dc4906490edd4e6e0d585fbe9bafcb4345a95249
dev_margooo <luckinamargo@gmail.com>
2024-06-29 18:25:21 +0300

    Добавляет тесты

--- src/test/java/ru/gazprombank/app/bot/mocks/AppConfigMock.java
+++ src/test/java/ru/gazprombank/app/bot/mocks/AppConfigMock.java
@@ -1,13 +1,9 @@
-package ru.gazprombank.app;
+package ru.gazprombank.app.bot.mocks;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
-public class AppConfig {
-    @Bean
+public class AppConfigMock {
     public RestTemplate restTemplate() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         factory.setConnectTimeout(this.timeout);
